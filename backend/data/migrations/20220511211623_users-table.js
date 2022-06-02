0257b28a28df1d1ba0ab57a56df08923c4de2535
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-06-02 12:31:02 -0400

    added a favorites table in db

--- backend/data/migrations/20220511211623_users-table.js
+++ backend/data/migrations/20220511211623_users-table.js
@@ -1,14 +1,20 @@
 exports.up = function (knex) {
-  return knex.schema.createTable("users", (tbl) => {
-    tbl.increments();
-    tbl.string("first_name").notNullable();
-    tbl.string("last_name").notNullable();
-    tbl.string("email").notNullable().unique();
-    tbl.string("username").notNullable().unique();
-    tbl.string("password").notNullable();
-  });
+  return knex.schema
+    .createTable("users", (tbl) => {
+      tbl.increments("user_id");
+      tbl.string("first_name").notNullable();
+      tbl.string("last_name").notNullable();
+      tbl.string("email").notNullable().unique();
+      tbl.string("username").notNullable().unique();
+      tbl.string("password").notNullable();
+    })
+    .createTable("favorites", (tbl) => {
+      tbl.increments("favorites_id");
+      tbl.string("coin_name");
+      tbl.integer("user_id").unsigned().notNullable().references("user_id").inTable("users");
+    });
 };
 
 exports.down = function (knex) {
-  return knex.schema.dropTableIfExists("users");
+  return knex.schema.dropTableIfExists("favorites").dropTableIfExists("users");
 };
