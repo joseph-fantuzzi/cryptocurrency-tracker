acbfbc72333952923b5936cc56452f2c089aeea7
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-06-21 16:23:17 -0400

    removed be and fe on same origin

--- backend/api/server.js
+++ backend/api/server.js
@@ -1,23 +1,16 @@
 const express = require("express");
 const helmet = require("helmet");
 const cors = require("cors");
-const path = require("path");
 
 const UsersRouter = require("./users/users-router");
 
 const server = express();
-
-server.use(express.static(path.resolve(__dirname, "../../frontend/build")));
 
 server.use(helmet());
 server.use(express.json());
 server.use(cors());
 
 server.use("/api/users", UsersRouter);
-
-server.get("/", (req, res) => {
-  res.sendFile(path.resolve(__dirname, "../../frontend/build", "index.html"));
-});
 
 server.use((err, req, res, next) => {
   res.status(err.status || 500).json({
