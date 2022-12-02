9242a541de5a1ab27e5e7fb7392e240cb9d8cee9
Joseph Fantuzzi <dev.josephfantuzzi@gmail.com>
2022-12-01 21:44:11 -0500

    added a chart in each coin page

--- frontend/src/components/IndividualCoin.js
+++ frontend/src/components/IndividualCoin.js
@@ -2,11 +2,25 @@ import React, { useState, useEffect } from "react";
 import axios from "axios";
 import { useParams } from "react-router-dom";
 import "../styles/styles.css";
-import Ticker from "./Ticker";
 import CircularProgress from "@mui/material/CircularProgress";
+import {
+  Chart as ChartJS,
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Filler,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import moment from "moment";
+
+ChartJS.register(CategoryScale, LinearScale, PointElement, LineElement, Filler, Title, Tooltip);
 
 const IndividualCoin = ({ toggleDark }) => {
   const [coin, setCoin] = useState(null);
+  const [coinData, setCoinData] = useState([]);
   const { itemID } = useParams();
 
   useEffect(() => {
@@ -18,12 +32,59 @@ const IndividualCoin = ({ toggleDark }) => {
       .catch((err) => {
         console.error(err);
       });
+
+    axios
+      .get(`https://api.coingecko.com/api/v3/coins/${itemID}/market_chart?vs_currency=usd&days=7`)
+      .then((res) => {
+        setCoinData(res.data.prices.map((value) => ({ x: value[0], y: value[1] })));
+      })
+      .catch((err) => {
+        console.error(err);
+      });
   }, [itemID]);
 
+  const options = {
+    responsive: true,
+    scales: {
+      x: {
+        grid: {
+          display: false,
+        },
+      },
+      y: {
+        grid: {
+          display: false,
+        },
+      },
+    },
+  };
+  const data = {
+    labels: coinData.map((value) => moment(value.x).format("MMM DD")),
+    datasets: [
+      {
+        fill: true,
+        label: itemID,
+        data: coinData.map((val) => val.y),
+        borderColor: "#52E6FA",
+      },
+    ],
+  };
+
   return coin ? (
-    <div className="w-3/4 mx-auto py-10 text-center flex flex-col items-center justify-center">
-      <h1 className="text-3xl pb-5">{coin.name}</h1>
-      <img className="w-96" src={coin.image.large} alt={coin} />
+    <div
+      className={`w-11/12 max-w-7xl mx-auto py-10 flex flex-col ${
+        toggleDark ? "text-white" : "text-black"
+      }`}
+    >
+      <div className="flex items-center gap-3">
+        <img className="w-20" src={coin.image.large} alt={coin} />
+        <h1 className="text-3xl">{coin.name}</h1>
+      </div>
+      <div className="mt-5 w-full h-1/2 hinline-block relative bg-[#000924] p-5 rounded-2xl">
+        <div className="w-full h-full flex justify-center">
+          <Line options={options} data={data} />
+        </div>
+      </div>
       <p>Sybmol: {coin.symbol}</p>
       <p>Market Cap Rank: {coin.market_cap_rank}</p>
       <p>Market Capitalization: {coin.market_data.market_cap.usd}</p>
@@ -55,12 +116,6 @@ const IndividualCoin = ({ toggleDark }) => {
       <p>Total Volume: {coin.market_data.total_volume.usd}</p>
       <p>24 Hour High: {coin.market_data.high_24h.usd}</p>
       <p>24 Hour Low: {coin.market_data.low_24h.usd}</p>
-      <div>
-        <h2>Exchanges</h2>
-        {coin.tickers.map((ticker, tickerID) => {
-          return <Ticker key={tickerID} ticker={ticker} />;
-        })}
-      </div>
     </div>
   ) : (
     <div className="outer-min-height text-4xl flex flex-col justify-center items-center">
